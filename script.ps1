8f1708c9ed37ab5dbbe3d5c1eea3af8bdc53a0de
Devon Slonaker <devons1@umbc.edu>
2023-09-24 06:29:20 -0400

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
 $URL = "https://i.kym-cdn.com/photos/images/original/001/742/441/847.jpg";
 $FILE = "C:\ProgramData\wallpaper.jpg";
-(New-Object System.Net.WebClient).DownloadFile($URL, $Path);
+Start-BitsTransfer -Source $URL -Destination $FILE;
 set-itemproperty -path "C:\ProgramData" -name WallPaper -value wallpaper.jpg;
