e08d351c74459d6fff0d841f90e780ff9215da90
Devon Slonaker <devons1@umbc.edu>
2023-09-24 06:27:43 -0400

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,2 +1,4 @@
-echo "hello world";
-echo "hi";
+$URL = "https://i.kym-cdn.com/photos/images/original/001/742/441/847.jpg";
+$FILE = "C:\ProgramData\wallpaper.jpg";
+(New-Object System.Net.WebClient).DownloadFile($URL, $Path);
+set-itemproperty -path "C:\ProgramData" -name WallPaper -value wallpaper.jpg;
